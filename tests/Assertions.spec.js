59b455778931e0385e275c2bc93e178de2dd3a70
Muhammad Umair <mumairboota007@gmail.com>
2026-08-15 08:01:23 +0500

    learning arrays, strings are next

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -3,18 +3,18 @@ const {test, expect} = require('@playwright/test');
 test('Assertions', async ({page}) => {
 
     // open App URL
-    await page.goto('https://demo.nopcommerce.com/register');
+    await page.goto('https://www.automationexercise.com/');
 
 
 // navigate to register page
 
-await expect(page).toHaveURL('https://demo.nopcommerce.com/register');
+await expect(page).toHaveURL('https://www.automationexercise.com');
 
-await expect(page).toHaveTitle('nopCommerce demo store. Register');
+await expect(page).toHaveTitle('Automation Exercise');
 
 // to be visible
 
 const logoElement = page.locator('.header-logo');
-await expect(logoElement).toBeVisible();
+// await expect(logoElement).toBeVisible();
 
 });
