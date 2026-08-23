30955cfbec1aa5bb34c0173b226e48ac1ec16a4f
Muhammad Umair <mumairboota007@gmail.com>
2026-08-23 18:16:19 +0500

    learning the login functionalties in the playwright using saucedemo application

--- tests/TestLoginFlow.spec.js
+++ tests/TestLoginFlow.spec.js
@@ -10,7 +10,7 @@ test('Login flow with valid credentials', async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
 
     await page.fill('#user-name', 'standard_user');
-    await page.locator('#password').fill('secret_sauce');
+    await page.locator('#password-123').fill('secret_sauce');
 
     await page.click('#login-button');
 
