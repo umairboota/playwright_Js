9f48cbe1ad6afce2a5466863239e83a3f4d24a0c
Muhammad Umair <mumairboota007@gmail.com>
2026-08-23 17:43:34 +0500

    learning the login functionalties in the playwright using saucedemo application

---
+++ tests/TestLoginFlow.spec.js
@@ -0,0 +1,21 @@
+import{test, expect} from '@playwright/test';
+
+test('Login Flow', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    expect(page).toHaveTitle('Swag Labs');
+
+});
+
+test('Login flow with valid credentials', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.fill('#user-name', 'standard_user');
+    await page.locator('#password').fill('secret_sauce');
+
+    await page.click('#login-button');
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+
+
+});
