72db6bf89381cb2357862f94136ac7e58740d81b
Muhammad Umair <mumairboota007@gmail.com>
2026-08-23 15:19:45 +0500

    learning the playwright

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -1,19 +1,19 @@
-const {test, expect} = require('@playwright/test');
+// const {test, expect} = require('@playwright/test');
 
-test('Home Page', async ({page}) => {
+// test('Home Page', async ({page}) => {
 
-    await page.goto('https://demoblaze.com/index.html');
+//     await page.goto('https://demoblaze.com/index.html');
 
-    const title = await page.title();
-    console.log('Page Title is:', title);
-    await expect(page).toHaveTitle('STORE');
+//     const title = await page.title();
+//     console.log('Page Title is:', title);
+//     await expect(page).toHaveTitle('STORE');
 
-    const url = page.url();
-    console.log('Page URL is:', url);
+//     const url = page.url();
+//     console.log('Page URL is:', url);
 
-    await expect(page).toHaveURL('https://demoblaze.com/index.html');
+//     await expect(page).toHaveURL('https://demoblaze.com/index.html');
     
-    await page.close();
+//     await page.close();
 
-});
+// });
 
