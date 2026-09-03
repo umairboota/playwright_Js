ef9ccaded98df5dd4fc94b5ea1ce83f6ea6e9117
Ali Jafri <ali.jafri@jigede.com>
2026-09-03 08:58:06 -0700

    learning traces

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,43 +1,34 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-/** @type {import('@playwright/test').BrowserContext} */
-let context;
-let page;
-test.beforeAll(async ({browser}) => {
+test.beforeAll(async ({ context }) => {
+  await context.tracing.start({
+    snapshots: true,
+    screenshots: true,
+    sources: true
+  });
+});
 
-  context = await browser.newContext();
-  await context.tracing.start(
-    {
-      snapshots: true,
-      screenshots: true,
-      sources: true
-    }
-  )
 
-page = await context.newPage();
-})
+test.afterAll(async ({ context }, testInfo) => {
+  await context.tracing.stop({
+    path: testInfo.outputPath('trace.zip')
+  });
+});
 
-test('has title', async ({page}) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-
-test('get started link', async ({page}) => {
+test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'Installation' })
+  ).toBeVisible();
 });
 
-
-test.afterAll(async () => {
-  await context.tracing.stop({ path: 'trace.zip' });
-
-})
