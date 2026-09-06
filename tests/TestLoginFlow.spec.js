b7cec3b4702f37638d5d017e913b70e962329483
Muhammad Umair <mumairboota007@gmail.com>
2026-09-06 23:33:31 +0500

    Locators and selectors learning

---
+++ tests/TestLoginFlow.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Flow', async ({ page }) => {
+
+    // navigate to Login page
+
+    await page.goto('https://www.saucedemo.com/');
+
+    // enter the username
+
+    await page.locator('#user-name').fill('standard_user');
+
+    // enter the password
+
+    await page.locator('#password').fill('secret_sauce');
+
+        // click the login button
+
+    await page.locator('#login-button').click();
+
+});
