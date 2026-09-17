1bb516ba7057ef8e30264f94f984ec3b25784597
Muhammad Umair <mumairboota007@gmail.com>
2026-09-17 12:51:44 +0500

    started learning the asserstions

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -1,11 +1,19 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Assertions', async ({page}) => {
+test('Assertions', async ({ page }) => {
 
     // open App URL
     await page.goto('https://kitchen.applitools.com/');
 
     await page.pause();
+
+    await expect(page.locator('text= The Kitchen')).toHaveCount(1);
+
+    if (await page.$('text= The Kitchen')) {
+        await expect(page.locator('text= The Kitchen')).toBeVisible();
+        await page.locator('text= The Kitchen').click();
+
+    }
 
 
 
