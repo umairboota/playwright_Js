65050fb2e4554077f126235187e323a35b743506
Muhammad Umair <mumairboota007@gmail.com>
2026-09-17 14:16:15 +0500

    started learning the asserstions

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -30,11 +30,30 @@ test('test visibility of elements', async ({ page }) => {
 })
 
 
-test(' test enabbled/disbaled elements', async ({page})=>{
+test(' test enabbled/disbaled elements', async ({ page }) => {
 
     await page.goto('https://kitchen.applitools.com/');
     await expect(page.locator('(//h3[@class="chakra-heading css-zey6tx"])[1]')).toBeEnabled();
 
     await expect.soft(page.locator('(//h3[@class="chakra-heading css-zey6tx"])[6]')).toBeDisabled();
+
+})
+
+
+test('test the value is matched or not?', async ({ page }) => {
+
+    await page.goto('https://kitchen.applitools.com/');
+
+    await expect(page.locator('text= The Kitchen')).toHaveText('The Kitchen');
+
+
+    await expect.soft(page.locator('text= The Kitchen')).not.toHaveText('The Kitchen2');
+
+
+
+})
+
+test('test the attributes', async ({page})=>{
+
 
 })
