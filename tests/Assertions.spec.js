f9ec2aaf7ae8d57ac3861fb4bf343ab652472f12
Muhammad Umair <mumairboota007@gmail.com>
2026-09-17 12:13:58 +0500

    started learning the asserstions

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -3,18 +3,10 @@ const {test, expect} = require('@playwright/test');
 test('Assertions', async ({page}) => {
 
     // open App URL
-    await page.goto('https://www.automationexercise.com/');
+    await page.goto('https://kitchen.applitools.com/');
+
+    await page.pause();
 
 
-// navigate to register page
 
-await expect(page).toHaveURL('https://www.automationexercise.com');
-
-await expect(page).toHaveTitle('Automation Exercise');
-
-// to be visible
-
-// const logoElement = page.locator('.header-logo');
-// await expect(logoElement).toBeVisible();
-
-});
+})
